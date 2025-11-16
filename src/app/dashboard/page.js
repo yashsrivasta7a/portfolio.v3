030a9254455331a7b4d9c701e209ad6462d29e3f
Yash Srivastava <ysyashmks@gmail.com>
2025-11-16 23:38:14 +0530

    Refactor layout and add DashboardPage component

---
+++ src/app/dashboard/page.js
@@ -0,0 +1,9 @@
+export default function DashboardPage() {
+  return (
+    <div>
+      <div className="ml-50% w-4xl bg-black border border-white/30"> 
+        hello
+      </div>
+    </div>
+  )
+}
